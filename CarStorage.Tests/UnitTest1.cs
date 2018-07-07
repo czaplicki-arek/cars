b3a6f4d3983ea43a344263ea4d0b32b4ed553a22
czaplicki-arek <40891534+czaplicki-arek@users.noreply.github.com>
2018-07-07 09:27:09 +0200

    Add files via upload

---
+++ CarStorage.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarStorage.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
